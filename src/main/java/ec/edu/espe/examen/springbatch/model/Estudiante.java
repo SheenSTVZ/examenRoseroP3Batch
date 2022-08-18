8791f5f2a216724e4648c0dc2367efd0de48f58f
SheenSTVZ <jhoandange@gmail.com>
2022-08-17 22:42:00 -0500

    first commit

---
+++ src/main/java/ec/edu/espe/examen/springbatch/model/Estudiante.java
@@ -0,0 +1,10 @@
+package ec.edu.espe.examen.springbatch.model;
+
+public class Estudiante {
+    private String id;
+    private String internalId;
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private Integer nivel;
+}
---
+++ src/main/java/ec/edu/espe/examen/springbatch/process/TareaUno.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.examen.springbatch.process;
+
+import ec.edu.espe.examen.springbatch.model.Estudiante;
+
+import java.util.List;
+
+public class TareaUno {
+    private List<Estudiante> estudiantes;
+}
